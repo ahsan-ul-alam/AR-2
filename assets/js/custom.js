dcaabce9d14859524b3af534a354884b778f2078
Ahsan-ul-alam <ahsanulalam.500@gmail.com>
2024-03-26 03:29:03 +0600

    Progressive site

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,8 @@ window.onscroll = ()=>{
     const percentage = ((scrollY/(documentHeight-viewportHeight))*100);
     filled.style.width = percentage + '%';
 }
+
+const header = document.querySelector('header');
+if(scrollY > 1){
+    header.style.backgroundColor = 'red';
+}
