b3723ee675f32ada7a2d2bd780fd288197a68c19
Ahsan-ul-alam <ahsanulalam.500@gmail.com>
2024-03-27 01:52:00 +0600

    Some new Feature

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,10 @@
 const filled = document.querySelector('.progressiveNav');
 const documentHeight = document.documentElement.scrollHeight;
 const viewportHeight = document.documentElement.clientHeight;
-window.onscroll = ()=>{
-    const percentage = ((scrollY/(documentHeight-viewportHeight))*100);
-    filled.style.width = percentage + '%';
+window.onload = () =>{
+    filled.style.width = 0;
 }
-
-const header = document.querySelector('header');
-if(scrollY > 1){
-    header.style.backgroundColor = 'red';
+window.onscroll = () => {
+    const percentage = ((scrollY / (documentHeight - viewportHeight)) * 100);
+    filled.style.width = percentage + '%';
 }
